c5f848db4fd37a5b2a02769e5a6b2a47c72bca0d
TopoX84 <leopatino1@gmail.com>
2019-05-07 14:41:06 -0500

    fixed working of welcome message

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ bot.on("guildMemberUpdate", function (oldMember, newMember) {
 
     if (settings.enforceRules == "yes" || settings.enforceRules == "Yes" || settings.enforceRules == "YES") {
         newMember.roles.forEach((role) => {
-            if (role.name === settings.enforceRulesRole) newMember.send("Welcome to **" + `${bot.guilds.get(settings.enforRulesServerID).name}` + "**, you must read the rules and make sure you understand them, once done type ||`!accept`||");
+            if (role.name === settings.enforceRulesRole) newMember.send("Welcome to **" + bot.guilds.get(settings.enforRulesServerID).name + "**, Before you can access all the other channels, You must read the rules on **" + settings.channelRulesName + "** channel and within that same channel type ||`!accept`||");
 
         });
     };
